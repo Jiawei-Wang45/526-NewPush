61f8d39b7d3ea80daf2af7857eeb1eac2de46be7
Jiawei-Wang45 <jwang085@usc.edu>
2025-10-28 14:26:46 -0700

    web

--- Assets/Scripts/Items/BaseItem.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class BaseItem : MonoBehaviour
-{
-    public virtual void OnTriggerEnter2D(Collider2D collision)
-    {
-        //place holder, nothing to do here.
-    }
-}
